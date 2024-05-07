519b99e285dd54a997aded12a5cc78abdc6973d2
Nata <n.shik2016@gmail.com>
2024-05-07 16:03:23 +0300

    Вынесла логику работы с Telegram в отдельный проект

--- BusinessTraining.Notifications/TelegramHelper.cs
+++ BusinessTraining.Notifications/TelegramHelper.cs
@@ -1,7 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Telegram.Bot;
 
-namespace BusinessTraining
+namespace BusinessTraining.Notifications
 {
     public class TelegramHelper
     {
--- BusinessTraining/AuthorizationForm.cs
+++ BusinessTraining/AuthorizationForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessTraining.Notifications;
+using System;
 using System.IO;
 using System.Windows.Forms;
 
